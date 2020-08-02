aa11887a70c3363623ca9486459f2d68eebe9fb5
Ryan Hanrahan <ryanhammerdick@gmail.com>
2020-08-01 19:31:37 -0700

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,14 @@
-def main():
-  print('You rolled a die')
+import random
+dice_rolls = 2
+dice_sum = 0
+for i in range(0,dice_rolls):
+ def main():
+  roll = random.randint(1,6)
+  dice_sum += roll
+  print(f'You rolled a {roll}')
+print(f'you have rolled a total of {dice_sum}')  
 
 if __name__== "__main__":
   main()
   
-dice_roller.py
-python /path/to/directory/dice_roller.py, where /path/to/directory
+
