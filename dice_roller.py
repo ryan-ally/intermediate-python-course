87bdeb30780a4407d3d83b62b9d9e7f7cbb4e167
Ryan Hanrahan <ryanhammerdick@gmail.com>
2020-08-01 18:28:27 -0700

    git add dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,8 @@
 def main():
-  #print('You rolled a die')
+  print('You rolled a die')
 
 if __name__== "__main__":
   main()
+  
+dice_roller.py
+python /path/to/directory/dice_roller.py, where /path/to/directory
